e3ba5d114bf0b1b2059fd591985fc9c3549a27db
Vijul Tyagi <92642078+vxt7373@users.noreply.github.com>
2021-11-29 14:35:47 -0600

    routing

--- sayitright/resources/views/home.blade.php
+++ sayitright/resources/views/home.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Say it Right</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+<head>
+    <title>Say it Right</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
 </head>
 
